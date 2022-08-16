cea99c139e4b980e313688d3fa4d67670a2d45af
appinha <amanda_pinha@hotmail.com>
2022-08-16 09:20:47 -0300

    [LeetCode] Adds 1-twoSum

---
+++ LeetCode/1-twoSum.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/two-sum/
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complementByNum = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in complementByNum:
+                return [i, nums.index(complement)]
+            complementByNum[num] = complement
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    inputs = [
+        [[2,7,11,15], 9],  # [0, 1]
+        [[3,2,4], 6],  # [1,2]
+        [[3,3], 6],  # [0,1]
+        [[-3,4,3,90], 0],  # [0, 2]
+    ]
+    for input in inputs:
+        print(solution.twoSum(*input))
