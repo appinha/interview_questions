1f7371b1f1b32f3bcb6eaff199eba0628e6e0490
appinha <amanda_pinha@hotmail.com>
2022-08-16 07:39:29 -0300

    [LeetCode] Adds optimized version to 234-isLinkedListPalindrome

--- LeetCode/234-isLinkedListPalindrome.py
+++ LeetCode/234-isLinkedListPalindrome.py
@@ -5,28 +5,51 @@ from helpers import ListNode, LinkedList
 
 
 class Solution:
+    # O(n) time and space
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         allValues = []
         currentNode = head
         while currentNode:
             allValues.append(currentNode.val)
             currentNode = currentNode.next
+        return allValues == allValues[::-1]
 
-        for i, value in enumerate(reversed(allValues)):
-            if allValues[i] != value:
-                return False
-        return True
+    # O(n) time and O(1) space
+    def isPalindrome2(self, head: Optional[ListNode]) -> bool:
+        length = LinkedList.len(head)
+        if length == 1:
+            return True
+        halfLength = length // 2
+        hasMiddleNode = length % 2 != 0
+
+        i = 1
+        j = halfLength
+        halfSum = 0
+        currentNode = head
+        while currentNode:
+            if hasMiddleNode and i == halfLength + 1:
+                pass
+            elif i <= halfLength:
+                halfSum += currentNode.val * (i + 1)
+            else:
+                halfSum -= currentNode.val * (j + 1)
+                j -= 1
+            i += 1
+            currentNode = currentNode.next
+
+        return halfSum == 0
 
 
 if __name__ == '__main__':
     solution = Solution()
     inputs = [
-        [1],
-        [2, 2],
-        [1, 2, 2, 1],
-        [1, 2, 3, 2, 1],
-        [1, 2, 3, 1],
+        [1],  # True
+        [2, 2],  # True
+        [1, 2, 2, 1],  # True
+        [1, 2, 3, 2, 1],  # True
+        [1, 3, 2, 4, 3, 2, 1],  # False
+        [1, 2, 3, 1],  # False
     ]
     for input in inputs:
         head = LinkedList.generate(input)
-        print(solution.isPalindrome(head))
+        print(solution.isPalindrome(head), solution.isPalindrome2(head))
